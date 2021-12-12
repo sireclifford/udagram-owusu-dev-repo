fd7bd8630b6f3feb5a3b6146bef907657c9e8809
Clifford Owusu <pounds84k@live.co.uk>
2021-12-12 11:08:50 +0000

    Initial commit

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "udagramgenixdev",
+    "password": "udagramgenixdev",
+    "database": "postgres",
+    "host": "udagramgenixdev.cqdaw8cie2yk.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-owusu-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
